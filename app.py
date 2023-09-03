399e9b661712e9955ef227d0bafd736fad55a8dd
Mohammad Safiuddin <40533383+safi842@users.noreply.github.com>
2023-09-04 03:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown("""
     **Description:**  
     This application serves as a tool for the analysis and prediction of exchange rates using time-series data. 
     It incorporates various analytical techniques including lag variable creation, seasonal decomposition, and variable selection through backward elimination. 
-    Users have the flexibility to customize each step of the analysis, visualize inherent patterns in the data.
+    Users have the flexibility to customize each step of the analysis and visualize inherent patterns in the data.
 """)
 
 # ... (rest of the code)
